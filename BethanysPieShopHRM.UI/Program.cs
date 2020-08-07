949480a402ddaf5cac11943c026329cb4f5d8bb1
Carl Björknäs <carl.bjorknas@knowit.se>
2020-08-07 10:46:24 +0200

    Added small clues about the error I get when entering url manually.

--- BethanysPieShopHRM.UI/Program.cs
+++ BethanysPieShopHRM.UI/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.Hosting;
 
 namespace BethanysPieShopHRM.UI
 {
+    // When manually entering an url that has a parameter I lose the styling and get the error below in the console
+    // GET https://localhost:44329/employeedetail/css/site.css net::ERR_ABORTED 404
+    // It's looking for site.css at wrong url?
+
     public class Program
     {
         public static void Main(string[] args)
